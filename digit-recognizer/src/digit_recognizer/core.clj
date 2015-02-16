dff67d360e983802a453ac9355144157231396af
Frank Burnham <fra.burnham@gmail.com>
2015-02-16 16:33:35 -0500

    partition 5 with 10k neighborhood is a decent trade for time vs accuracy in clojure

--- digit-recognizer/src/digit_recognizer/core.clj
+++ digit-recognizer/src/digit_recognizer/core.clj
@@ -3,58 +3,39 @@
             [clojure.string :as str])
   (:gen-class))
 
-;num training cases total in train.csv 42000
-;40k training 2k testing
-
-(defmacro n? [r]
-  `(if (contains? ~r :n) (inc (:n ~r)) 2))
-
 (defn parse-data [data]
-  (map #(Integer/parseInt %) data))
-
-(defn cma [n last-cma x]
-  (/ (+ x (* n last-cma)) (inc n)))
-
-(defn avg-data [neighborhood]
-  (map
-    #(reduce
-      (fn [r x]
-        {:class (:class x)
-         :features (map (partial cma (n? r)) (:features r) (:features x))
-         :n (n? r)})
-      %)
-    (map #(get neighborhood %)
-         (keys neighborhood))))
+  (flatten (map (partial reduce +)
+                (partition-all 5 (map #(Integer/parseInt %) data)))))
 
 (defn build-neighborhood [rdr]
-  ;now group by class and average
-  (group-by :class
-            (map (fn [line]
-                   (let [[label & data] (str/split line #",")]
-                     {:class label
-                      :features (parse-data data)}))
-                 (take 15000 (rest (line-seq rdr))))))
+  (map (fn [line]
+         (let [[label & data] (str/split line #",")]
+           {:class label
+            :features (parse-data data)}))
+       (take 10000 (rest (line-seq rdr)))))
 
 (defn classify [rdr neighborhood]
-  (doall
-    (map (fn [line]
-           (let [[answer & data] (str/split line #",")]
-             [(:class (knn/classify 1 neighborhood (parse-data data)))
-              answer]))
-         (take 100 (line-seq rdr)))))
+  (map (fn [line]
+         (let [data (str/split line #",")]
+           (:class (knn/classify 1 neighborhood (parse-data data)))))
+       (rest (line-seq rdr))))
 
-(defn get-accuracy [results]
-  (/ (reduce +
-             (map (fn [[prediction actual]]
-                    (if (= prediction actual) 1 0))
-                  results))
-     (count results)))
+(defn file-dump [results]
+  (with-open [dump (clojure.java.io/writer "submission.csv")]
+    (.write dump (str "ID,label\n"))
+    (loop [res results
+           id 1]
+      (if (not (empty? res))
+        (do
+          (.write dump (str id "," (first (first res)) "\n"))
+          (recur (rest res) (inc id)))))))
 
 (defn -main
   "Parse training data from train.csv to build k-nn model for test.csv"
   [& args]
   (time
-    (with-open [rdr (clojure.java.io/reader "train.csv")]
-      (let [neighborhood (avg-data (build-neighborhood rdr))
-            results (classify rdr neighborhood)]
-        (println (get-accuracy results))))))
+    (with-open [train (clojure.java.io/reader "train.csv")
+                test (clojure.java.io/reader "test.csv")]
+      (let [neighborhood (build-neighborhood train)
+            results (classify test neighborhood)]
+        (file-dump results)))))
