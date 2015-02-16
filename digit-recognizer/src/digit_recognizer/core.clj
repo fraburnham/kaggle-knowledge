f2657a957ce5bb088155e5e3c91e1948cd0ca012
Frank Burnham <fra.burnham@gmail.com>
2015-02-16 13:05:10 -0500

    First test using raw pixel data as features. Algo is slow. Accuracy is good... so far

--- digit-recognizer/src/digit_recognizer/core.clj
+++ digit-recognizer/src/digit_recognizer/core.clj
@@ -1,7 +1,31 @@
 (ns digit-recognizer.core
+  (:require [k-nn.core :as knn]
+            [clojure.string :as str])
   (:gen-class))
 
+;num training cases total in train.csv 42000
+;40k training 2k testing
+
+
+(defn build-neighborhood [rdr]
+  (map (fn [line]
+         (let [[label & data] (str/split line #",")]
+           {:class label
+            :features (map #(Integer/parseInt %) data)}))
+       (take 40000 (rest (line-seq rdr)))))
+
+(defn classify [rdr neighborhood]
+  (doall
+    (map (fn [line]
+           (let [[answer & data] (str/split line #",")]
+             (println "Prediction: " (knn/classify 1 neighborhood
+                                                   (map #(Integer/parseInt %) data))
+                      "Actual: " answer))) (line-seq rdr))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Parse training data from train.csv to build k-nn model for test.csv"
   [& args]
-  (println "Hello, World!"))
+  (time
+    (with-open [rdr (clojure.java.io/reader "train.csv")]
+      (let [neighborhood (build-neighborhood rdr)]
+        (classify rdr neighborhood)))))
